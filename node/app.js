2930ed01d705bf86ad3edc83a5a3b7be9b08f98a
bski006 <boch@umich.edu>
2015-09-26 03:23:29 -0700

    test

--- node/app.js
+++ node/app.js
@@ -21,3 +21,26 @@ io.of('/stream_classify')
 		})
 		.on ('disconnect', function () {})
  	}) 
+
+app.post ('/notify', function (req, res) {
+    var client = io.sockets[req.body.socket_id]
+    client.emit ('classification_result', req.body.classification_result)
+    res.type ('text/plain')
+    res.send ('Response Sent To Mobile socket[' + req.body.socket_id + ']')
+})
+
+app.get ('/classifications', function (req, res) {
+    util.connect_mongo (function (err, mongoClient) {
+    var clz_coll = mongoClient.db ('hdd').collection ('classifications')
+        clz_coll.find ({'tester_id': req.params.tester_id})
+                .limit(req.params.pagesize)
+                .skip(req.params.pagenum * req.params.pagesize)
+                .sort ({'date_created': -1})
+                .toArray (function (err, results) {
+                    if (err)
+                        res.status (500)
+                    else
+                        res.json ({classifications: results})
+                })
+    })
+})
