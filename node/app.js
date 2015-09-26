71a8d3f196bf73b8e9940983ff896e9c38122131
Ubuntu <ubuntu@ip-172-31-31-254.us-west-2.compute.internal>
2015-09-26 09:31:55 +0000

    init

--- node/app.js
+++ node/app.js
@@ -1,18 +1,23 @@
-var app = require ('express').createServer(),
-    io = require ('socker.io')(app),
+var app = require ('express')(),
+    server = require ('http').createServer(app),
+    io = require ('socker.io')(server),
     bodyParser = require ('body-parser')
 
 app.use (bodyParser.urlencoded ({ extended: true }))
-app.listen (80)
+server.listen (80)
 
 io.of('/stream_classify')
-	.on ('connection', function (client) {
-  	
-   	})
-	.on ('raw_image_data', function (data) {
-		/* reads image as a buffer */
-		
-	})
-	.on ('image_s3_url', function (url) {
-	})
-  
+	.on ('connection', function (socket) {
+		socket.on ('raw_image_data', function (id, msg) {
+		/* emit the register event, give client its id */
+			client.emit ('register', id)  	
+		})
+		.on ('raw_image_data', function (id, data) {
+			/* reads image as a buffer */
+			/* try pass the original id for testing */
+			
+		})
+		.on ('image_s3_url', function (id, url) {
+		})
+		.on ('disconnect', function () {})
+ 	}) 
