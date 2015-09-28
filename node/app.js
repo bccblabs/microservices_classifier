a3961a8f3108a29f5d64cfad9749c4f6e3260120
bski006 <boch@umich.edu>
2015-09-27 23:12:13 -0700

    stuff

--- node/app.js
+++ node/app.js
@@ -1,29 +1,48 @@
 var app = require ('express')(),
     server = require ('http').createServer(app),
-    io = require ('socker.io')(server),
-    bodyParser = require ('body-parser')
+    io = require ('socket.io')(server),
+    fs = require ('fs'),
+    bodyParser = require ('body-parser'),
+    file_path = '/tmp/hdd_uploads',
+    temp = require ('temp')
+
 
 app.use (bodyParser.urlencoded ({ extended: true }))
 server.listen (80)
+temp.track()
 
-io.of('/stream_classify')
-	.on ('connection', function (socket) {
-		socket.on ('raw_image_data', function (id, msg) {
+//io.of('/stream_classify')
+io.on ('connection', function (socket) {
+    socket.on ('connection', function (id, msg) {
 		/* emit the register event, give client its id */
-			client.emit ('register', id)  	
-		})
-		.on ('raw_image_data', function (id, data) {
+        var socket_id = { socket_id: id}
+		client.emit ('register', socket_id)  	
+	})
+	.on ('clz_data', function (id, data) {
+        console.log (id + ": " + data)
 			/* reads image as a buffer */
 			/* try pass the original id for testing */
-			
-		})
-		.on ('image_s3_url', function (id, url) {
-		})
-		.on ('disconnect', function () {})
- 	}) 
+            // temp.open ({dir: file_path, suffix: '.jpg'}, function (err, info) {
+            //     if (err)
+            //         throw err;
+            //     fs.write (info.fd, data)
+            //     fs.close (info.fd, function (err) {
+            //         if (err) throw err
+            //         /* passing info.path on to the queue */
+            //     })
+            // })
+	})
+	.on ('image_s3_url', function (id, url) {
+            /* passing s3.url directly onto the queue */
+	})
+	.on ('disconnect', function () {
+        
+    })
+}) 
 
 app.post ('/notify', function (req, res) {
     var client = io.sockets[req.body.socket_id]
+    console.log (client.sockets)
     client.emit ('classification_result', req.body.classification_result)
     res.type ('text/plain')
     res.send ('Response Sent To Mobile socket[' + req.body.socket_id + ']')
