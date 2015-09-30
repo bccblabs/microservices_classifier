67f1836567d32c2396049645431be97527e843fa
Ubuntu <ubuntu@ip-172-31-31-254.us-west-2.compute.internal>
2015-09-30 07:35:46 +0000

    code

--- node/app.js
+++ node/app.js
@@ -52,18 +52,18 @@ app.post ('/notify', function (req, res) {
     res.send ('Response Sent To Mobile socket[' + req.body.socket_id + ']')
 })
 
-app.get ('/classifications', function (req, res) {
+app.post ('/classifications', function (req, res) {
     util.connect_mongo (function (err, mongoClient) {
-    var clz_coll = mongoClient.db ('hdd').collection ('classifications')
-        clz_coll.find ({'tester_id': req.params.tester_id})
-                .limit(req.params.pagesize)
-                .skip(req.params.pagenum * req.params.pagesize)
+    var clz_coll = mongoClient.db ('hdd').collection ('classifications'),
+	query_obj = {}
+        clz_coll.find (query_obj)
+                .limit(req.body.pageSize)
                 .sort ({'date_created': -1})
                 .toArray (function (err, results) {
                     if (err)
                         res.status (500)
                     else
-                        res.json ({classifications: results})
+                        res.json ({samples: results})
                 })
     })
 })
