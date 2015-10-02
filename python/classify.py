90da209ec3246aa1c12ef2433b3aec3b9fc321cf
bski006 <boch.006@gmail.com>
2015-10-02 00:17:55 +0000

    stuf

--- python/classify.py
+++ python/classify.py
@@ -1,18 +1,18 @@
 from celery import Celery, Task
 import time, requests, pickle, caffe
-# from caffe import Net, SGDSolver
-# caffe.set_mode_cpu()
-caffe_root = "/Users/bski/Project/caffe/"
-data_root = caffe_root + "data/"
-# blob = caffe.proto.caffe_pb2.BlobProto()
-labels = list( pickle.load (open(caffe_root + "models/trained/labels_450.p")))
-# hdd_classifier = caffe.Classifier (
-#                caffe_root + "models/trained/cars_450_deploy.prototxt",
-#                 caffe_root + "models/trained/cars_848_ft_ss40k_iter_100000.caffemodel",
-#                 channel_swap = (2,1,0),
-#                 raw_scale = 255,
-#                 image_dims = (256, 256)
-# )
+import numpy as np
+from caffe import Net, SGDSolver
+caffe.set_mode_cpu()
+caffe_root = "/home/ubuntu/caffe/"
+models_root = caffe_root + "models/hdd_13_ft/"
+labels = np.loadtxt(models_root + "labels.txt", str, delimiter="\t")
+hdd_classifier = caffe.Classifier (
+                models_root + "deploy.prototxt",
+                models_root + "hdd_13_iter_25k.caffemodel",
+                channel_swap = (2,1,0),
+                raw_scale = 255,
+                image_dims = (256, 256)
+)
 print '[classifier] labels loaded ' + str (labels) 
 
 
@@ -23,7 +23,6 @@ class NotifierTask (Task):
     abstract = True
     def after_return (self, status, retval, task_id, args, kwargs, einfo):
         url = 'http://localhost:80/notify'
-        # needs to retrieve the id from mq msg, leave out here
         data = {'socket_id': 0, 'classification_result': retval}
         requests.post (url, data)
 
