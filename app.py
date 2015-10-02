f69de85d2ff55e756ce24c061ecdcfeebbb89846
bski006 <boch.006@gmail.com>
2015-10-02 02:08:27 +0000

    workflow alfa

--- app.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from celery import Celery
-from classify.tasks import classify
-app = Celery ('classify', broker='amqp://', backend='amqp://', include=['classify.tasks'])
-
-app.start()
