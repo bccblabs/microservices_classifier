1bafe7c0b740ece94c00b579ba7c2a580740c55c
bski006 <boch@umich.edu>
2015-11-09 00:39:16 -0800

    retry added

--- server/app.js
+++ server/app.js
@@ -6,75 +6,99 @@ var app = require ('express')(),
 var server = require ('http').createServer(app).listen(8080),
     io = require ('socket.io').listen(server),
     util = require ('./util'),
-    amqp = require ('amqplib/callback_api'),
     _ = require ('underscore-node'),
     async = require ('async'),
-    temp = require ('temp'),
-    AMQP_ADDR = process.env['RABBITMQ_PORT_5672_TCP_ADDR'] || 'localhost',
-    AMQP_PORT = process.env['MICROSERVICESCLASSIFIER_RABBITMQ_1_PORT_5672_TCP_PORT'] || '5672'
+    temp = require ('temp')
+
+
+var conn_amqp_wrapper = function (callback, results) {
+    var channel = "",
+        car_exchange = 'cars',
+        channel_opts = {durable: true},
+        AMQP_HOST = process.env['RABBITMQ_PORT_5672_TCP_ADDR'] || 'localhost',
+        AMQP_PORT = process.env['MICROSERVICESCLASSIFIER_RABBITMQ_1_PORT_5672_TCP_PORT'] || '5672',
+        amqp_addr = 'amqp://' + AMQP_HOST + ':' + AMQP_PORT
+
+    var conn_amqp = function (ex, ex_type, channel_opts, amqp_addr, callback) {
+        require ('amqplib/callback_api').connect (amqp_addr, function (err, conn) {
+            if (err) {
+                console.error (err)
+                console.log ("amqp conn error")
+                callback (err, null)
+            } else {
+                conn.createChannel (function (err, ch) {
+                    ch.assertExchange(ex, ex_type, channel_opts, function(err, ok) {
+                        if (err) {
+                            console.error ('amqp channel creation err')
+                            console.error (err)
+                            callback (err, null)
+                        } else {
+                            channel = ch
+                            callback (null, channel)
+                        }
+                    })
+
+                })
+
+            }
+        })
+    }(car_exchange, 'topic', channel_opts, amqp_addr, callback)
+}
+
 
 console.log ("Socket server listening on 8080")
 temp.track()
 
-var channel = "",
-    car_exchange = 'cars',
-    channel_opts = {durable: true}
-
-amqp.connect ('amqp://' + AMQP_ADDR + ':' + AMQP_PORT, function (err, conn) {
+async.retry ({times: 10, interval: 1000}, conn_amqp_wrapper, function (err, channel) {
     if (err) {
-	console.log (err)
-        console.log ("amqp conn error")
+        console.error (err)
+        console.log ("amqp conn error, exiting")
+        process.exit()
     } else {
-    	conn.createChannel (function (err, ch) {
-        	ch.assertExchange(car_exchange, 'topic', channel_opts, function(err, ok) {
-        		if (err) {
-				console.log (err)
-				console.log ('amqp channel creation err')
-        		} else {
-        			channel = ch
-        		}
-    		})
+        console.log ("[## rabbitmq connected ##]")
+        io.sockets.on ('connection', function (client) {
+            client.emit ('register', client.id)
 
-    	})
+            client.on ('clz_data', function (data) {
+                data = JSON.parse (data)
+                var mongo_store_minitask = function (callback) {
+                    util.store_to_mongo (data, callback)
+                }
+
+                var local_store_minitask = function (callback) {
+                    util.store_to_disk (data, callback, temp)
+                }
+
+                async.parallel ([mongo_store_minitask, local_store_minitask], function (err, res) {
+                    console.log ("init task returned")
+                    if (err) {
+                        console.log (err)
+                        client.emit ('err', 'init_task_error')                    
+                    } else {
+                        var channel_msg = {
+                            socket_id: client.id,
+                            object_id: _.first(_.filter(_.pluck (res, 'object_id'), function (val) {return val!==null && val!== undefined})), 
+                            file_path: _.first(_.filter(_.pluck (res, 'tmp_path'), function (val) {return val !== null && val !== undefined})),
+                            api_query: _.first(_.filter(_.pluck (res, 'query'), function (val) {return val !== null && val !== undefined}))
+                        }
+                        channel.publish ('cars', 'classify', new Buffer (JSON.stringify(channel_msg)))
+                        channel.publish ('cars', 's3', new Buffer (JSON.stringify(channel_msg)))
+                    }
+               })
+            })
+
+            client.on ('disconnect', function () {
+                console.log ("client " + client.id + " disconnected")
+            })
+        }) 
 
     }
 })
 
-io.sockets.on ('connection', function (client) {
-	client.emit ('register', client.id)
 
-	client.on ('clz_data', function (data) {
-		data = JSON.parse (data)
-        var mongo_store_minitask = function (callback) {
-            util.store_to_mongo (data, callback)
-        }
 
-        var local_store_minitask = function (callback) {
-            util.store_to_disk (data, callback, temp)
-        }
 
-        async.parallel ([mongo_store_minitask, local_store_minitask], function (err, res) {
-    		console.log ("init task returned")
-            if (err) {
-    		    console.log (err)
-                client.emit ('err', 'init_task_error')                    
-            } else {
-                var channel_msg = {
-                    socket_id: client.id,
-                    object_id: _.first(_.filter(_.pluck (res, 'object_id'), function (val) {return val!==null && val!== undefined})), 
-                    file_path: _.first(_.filter(_.pluck (res, 'tmp_path'), function (val) {return val !== null && val !== undefined})),
-                    api_query: _.first(_.filter(_.pluck (res, 'query'), function (val) {return val !== null && val !== undefined}))
-                }
-                channel.publish (car_exchange, 'classify', new Buffer (JSON.stringify(channel_msg)))
-                channel.publish (car_exchange, 's3', new Buffer (JSON.stringify(channel_msg)))
-            }
-       })
-    })
 
-	client.on ('disconnect', function () {
-        console.log ("client " + client.id + " disconnected")
-    })
-}) 
 
 app.post ('/notify', function (req, res) {
     util.fetch_listings (   {
