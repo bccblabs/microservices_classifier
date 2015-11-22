118150ee5a24b6c21153c19d6d4d90c4e63a25ee
bski006 <boch@umich.edu>
2015-11-21 17:52:57 -0800

    query: when fetching sort/filter, use 'remaining/fetched submodels in case of confusion of model names in db'

--- server/util.js
+++ server/util.js
@@ -142,7 +142,7 @@ var parse_car_query = function (query_params, min_price, max_price, sort_query) 
     }
 
     if (_.has (query_params, 'models') && query_params.models.length > 0) {
-        query['model'] = {'$in': parse_model(query_params.models)}
+        query['submodel'] = {'$in': parse_model(query_params.models)}
     }
 
     if (_.has (query_params, 'bodyTypes') && query_params.bodyTypes.length > 0) {
