a41c30a646e9e3bd2254fecc6f6719fe518b731b
bski006 <boch@umich.edu>
2015-11-22 15:39:33 -0800

    cars

--- server/app.js
+++ server/app.js
@@ -122,9 +122,9 @@ app.get ('/vehicle_info', function (req, res) {
 })
 
 app.post ('/listings', function (req, res) {
-    console.dir (res.body.api)
     var listings_query = util.parse_listings_query (req.body.api),
         cars_query = util.parse_car_query (req.body.car, req.body.min_price, req.body.max_price, req.body.sortBy)
+    console.dir (res.body)
     this.res = res
     this.body = req.body
     util.fetch_listings (cars_query, listings_query, util.listings_request_callback.bind (this))
