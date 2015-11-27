8753dbc00a9d129c16442bf075c79e5a27458915
bski006 <boch@umich.edu>
2015-11-26 16:36:18 -0800

    logging

--- server/app.js
+++ server/app.js
@@ -160,7 +160,7 @@ app.post ('/listings', function (req, res) {
     var listings_query = util.parse_listings_query (req.body.api),
         cars_query = util.parse_car_query (req.body.car, req.body.min_price, req.body.max_price, req.body.sortBy)
     console.log ('[* server] app query:')
-    console.dir (res.body)
+    console.dir (req.body)
     this.res = res
     this.body = req.body
     util.fetch_listings (cars_query, listings_query, util.listings_request_callback.bind (this))
