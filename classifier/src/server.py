43b912e3565ee3a6154f91a8908d9702260bf1b9
bski006 <boch@umich.edu>
2015-12-02 18:29:56 -0800

    post

--- classifier/src/server.py
+++ classifier/src/server.py
@@ -45,37 +45,37 @@ classifiers = [c0]
 
 @app.route("/classify")
 def classify():
-    try:
-        num_outs = 525
-        image_path = request.args.get ("image_path")
-    	print image_path
-        print ("[classifier] image loaded to " + image_path)
+    # try:
+    num_outs = 525
+    image_path = request.args.get ("image_path")
+	print image_path
+    print ("[classifier] image loaded to " + image_path)
 
-        image = center_crop (caffe.io.load_image(IMAGE_ROOT + image_path))
-        resized_image = caffe.io.resize_image (image, (256,256,3))
-        res = np.zeros (num_outs * len (classifiers)).reshape (num_outs, len(classifiers))
-        for i, x in enumerate (classifiers):
-            res[:,i] = x.predict ([resized_image])[0]
-        avg_probs = np.average (res, axis=1)
-        top_k_idx = avg_probs.argsort()[-1:-6:-1]
-        class_res = {}
-        class_res['top_3'] = []
-        for x in top_k_idx.tolist():
-            res_dict = {}
-            res_dict["class_name"] = labels.tolist()[x][0]
-            res_dict["prob"] = avg_probs.tolist()[x]
-            app.logger.debug (str(res_dict) + " " + image_file_name)
-            class_res['top_3'].append (res_dict)
-        class_res['top_1'] = class_res['top_3'][0]
-        app.logger.debug ("[classifier] classification result saved." + str(class_res.to_json()))
-        response = Response (response = class_res.to_json(), status=200, mimetype="application/json")
-        return response
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        app.logger.error (" ".join(traceback.format_tb (exc_traceback)))
-        resp = jsonify ({"msg": "Server Error"})
-        resp.status_code = 500
-        return resp
+    image = center_crop (caffe.io.load_image(IMAGE_ROOT + image_path))
+    resized_image = caffe.io.resize_image (image, (256,256,3))
+    res = np.zeros (num_outs * len (classifiers)).reshape (num_outs, len(classifiers))
+    for i, x in enumerate (classifiers):
+        res[:,i] = x.predict ([resized_image])[0]
+    avg_probs = np.average (res, axis=1)
+    top_k_idx = avg_probs.argsort()[-1:-6:-1]
+    class_res = {}
+    class_res['top_3'] = []
+    for x in top_k_idx.tolist():
+        res_dict = {}
+        res_dict["class_name"] = labels.tolist()[x][0]
+        res_dict["prob"] = avg_probs.tolist()[x]
+        app.logger.debug (str(res_dict) + " " + image_file_name)
+        class_res['top_3'].append (res_dict)
+    class_res['top_1'] = class_res['top_3'][0]
+    app.logger.debug ("[classifier] classification result saved." + str(class_res.to_json()))
+    response = Response (response = class_res.to_json(), status=200, mimetype="application/json")
+    return response
+    # except:
+    #     exc_type, exc_value, exc_traceback = sys.exc_info()
+    #     app.logger.error (" ".join(traceback.format_tb (exc_traceback)))
+    #     resp = jsonify ({"msg": "Server Error"})
+    #     resp.status_code = 500
+    #     return resp
 
 if __name__ == "__main__":
     if not app.debug:
