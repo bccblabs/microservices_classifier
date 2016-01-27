6dd5bfd5fc7011ed636e430896510db48c16f994
bski006 <boch@umich.edu>
2016-01-27 02:43:47 -0800

    inventories updataed

--- server/parser.js
+++ server/parser.js
@@ -107,7 +107,7 @@ var parse_car_query = function (query_params, min_price, max_price, sort_query) 
     if (_.has (query_params, 'compressors') && query_params.compressors.length > 0) {
         console.log ('[parser.parse_car_query]: compressors array\n')
         console.log (JSON.stringify (query_params['compressors'], null, 2))
-        query['powertrain.engine.compressorType'] = {'$in': query_params.compressors}
+        query['powertrain.engine.compressorType'] = {'$in': _.uniq (query_params.compressors)}
     }
     if (_.has (query_params, 'cylinders') && query_params.cylinders.length > 0) {
         query['powertrain.engine.cylinder'] = {'$in': query_params['cylinders']}
