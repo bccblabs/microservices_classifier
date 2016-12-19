b6f0053f68ff99aa961ee291e3c43e5d6ea8cb24
bski006 <boch.006@gmail.com>
2016-12-19 01:19:16 -0500

    Delete find_mongo.js

--- elastic/find_mongo.js
+++
@@ -1,8 +0,0 @@
-conn = new Mongo();
-db = conn.getDB("vehicle_data")
-
-// cursor = db.equipments.find().limit("20");
-cursor = db.style_infos.find({$where: 'this.options.length > 1'}, {'options': 1}).limit(50);
-while (cursor.hasNext()) {
-    printjson (cursor.next());
-}
